467b600e08308a55b9266ea98691eaa7c731b9fc
Emma Viitanen <149111850+EmmaViitanen@users.noreply.github.com>
2025-10-06 15:17:26 +0200

    added song

--- sketch.js
+++ sketch.js
@@ -1,15 +1,22 @@
+let song;
 function preload() {
   // Ladda en .obj eller .stl fil (måste ligga i projektmappen)
   heart = loadModel("Heart.obj", true);
+  song = loadSound("sza.mp3");
 }
 
 function setup() {
   createCanvas(innerWidth, innerHeight, WEBGL);
   frameRate(20);
   colorMode(RGB);
+  song.play();
 }
 
 function draw() {
   drawBackground();
   drawArt();
+}
+
+function mousePressed() {
+  song.play();
 }
