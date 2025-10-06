8a89d2f60c87d4d0e8c8a794aa71747a0c529468
olne23pf <nellieolssonwennberg@gmail.com>
2025-10-06 15:34:06 +0200

    upd

--- art.js
+++ art.js
@@ -9,19 +9,22 @@ let radius = 150;
 let numRings = 20;
 let counter = 0;
 
-// function preload() {
-//   // Ladda en .obj eller .stl fil (måste ligga i projektmappen)
-//   heart = loadModel("Heart.obj", true);
-// }
+function preload() {
+  heart = loadModel("Heart.obj", true);
+}
 
-// function setup() {
-//   createCanvas(innerWidth, innerHeight, WEBGL);
-//   colorMode(RGB);
-// }
+function setup() {
+  createCanvas(innerWidth, innerHeight, WEBGL);
 
-function drawArt() {
-  //   background(0, 0, 0);
-  //   clear();
+  // Skapa en offscreen-canvas för texturen
+  tex = createGraphics(400, 400);
+  tex.noFill();
+  tex.stroke(0);
+  tex.strokeWeight(1);
+}
+
+function draw() {
+  background(200);
 
   // --- RITA TEXTUREN PÅ OFFSCREEN-CANVASEN ---
   tex.background(255);
@@ -34,7 +37,13 @@ function drawArt() {
     for (let a = 0; a <= TAU; a += TAU / numPoints) {
       let x = r * cos(a);
       let y = r * sin(a);
-      let n = map(noise(x * resolution, y * resolution), 0, 1, -scaleNoise, scaleNoise);
+      let n = map(
+        noise(x * resolution, y * resolution),
+        0,
+        1,
+        -scaleNoise,
+        scaleNoise
+      );
       tex.curveVertex(x + n, y + n);
     }
     tex.endShape(CLOSE);
